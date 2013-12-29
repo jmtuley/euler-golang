9261da21ddc345b10ea9cd18deba4ff2715cffcf
John Tuley <john@tuley.org>
2013-12-28 21:37:38 -0700

    Add solution runner

---
+++ euler.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "fmt"
+  "github.com/jmtuley/euler-golang/problem_1"
+  "github.com/jmtuley/euler-golang/problem_2"
+)
+
+func main() {
+  fmt.Printf("Problem 1's solution is %v\n", problem_1.Solution())
+  fmt.Printf("Problem 2's solution is %v\n", problem_2.Solution())
+}
