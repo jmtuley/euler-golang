9352454eb3eeb9466490a07ba7e75100732ca0be
John Tuley <john@tuley.org>
2013-12-30 19:59:26 -0700

    Ensure that we really did find the max

--- problem_4/solution.go
+++ problem_4/solution.go
@@ -1,16 +1,23 @@
 package problem_4
 
-import "strconv"
+import (
+  "strconv"
+)
 
 func Solution() int {
   return LargestPalindromicProduct(999)
 }
 
 func LargestPalindromicProduct(factorMax int) int {
+  max := 0
   for i := factorMax; i > 0; i-- {
+    if max > i*i {
+      return max
+    }
+
     for j := i; j > 0; j-- {
-      if isPalindrome(i * j) {
-        return i * j
+      if isPalindrome(i*j) && i*j > max {
+        max = i * j
       }
     }
   }
